ea5f7b250a684650bf315f6894bdd142491e4b61
mlabouardy <mohamed.labouardy@gmail.com>
2015-12-13 18:54:07 +0100

    login and sign up routes

---
+++ public/app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name publicApp
+ * @description
+ * # publicApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('publicApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
+      .when('/sign-up', {
+        templateUrl: 'views/sign-up.html',
+        controller: 'SignUpCtrl',
+        controllerAs: 'signUp'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ public/app/scripts/controllers/sign-up.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name publicApp.controller:SignUpCtrl
+ * @description
+ * # SignUpCtrl
+ * Controller of the publicApp
+ */
+angular.module('publicApp')
+  .controller('SignUpCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
