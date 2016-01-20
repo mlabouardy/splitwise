e95575184cc9ce9611839e481652508141d04ed1
skyazid <hassaine.yazid.univ@gmail.com>
2016-01-20 15:31:05 +0100

    ajout du postinstall => npm run postinstall

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # splitwise
 M2 Web project
+
+## First user :
+
+username = "user_1@gmail.com"
+password = "user_1"
+
+## Second user :
+
+username = "user_2@gmail.com"
+password = "user_2"
--- postinstall.js
+++ postinstall.js
@@ -1,1 +1,93 @@
-//To be completed 
+//npm run postinstall
+
+var User = require('./models/user.js');
+var Group = require('./models/group.js');
+var Bill = require('./models/bill.js');
+var constants = require('./constants');
+var mongoose = require('mongoose');
+var md5 = require('md5');
+
+mongoose.connect(constants.DATABASE_HOST, function() {
+	
+	mongoose.connection.db.dropDatabase()
+
+	var user = new User()
+	user.email = "user_1@gmail.com"
+	user.password = md5("user_1")
+	user.firstName = "user_1 firstName"
+	user.lastName = "user_1 lastName"
+	user.save();
+
+	var friend = new User()
+	friend.email = "user_2@gmail.com"
+	friend.password = md5("user_2")
+	friend.firstName = "user_2 firstName"
+	friend.lastName = "user_2 lastName"
+	friend.friends.push(user._id);
+	friend.save();
+
+	user.friends.push(friend._id);
+	user.save();
+
+	new Group({
+		name: "Groupe 1",
+		owner: user._id,
+		bills: [{
+			"description": "Facture payée",
+			"price": 500,
+			"paid": true,
+			"paid_list": [{
+				user: user,
+				price: 300
+			}, {
+				user: friend,
+				price: 200
+			}],
+			"date": new Date().setDate(new Date().getDate() - 30)
+		}, {
+			"description": "Facture d'aujourd'hui",
+			"price": 100,
+			"paid": false,
+			"paid_list": [{
+				user: user,
+				price: 0
+			}, {
+				user: friend,
+				price: 0
+			}],
+			"date": new Date().setDate(new Date().getDate())
+		}, {
+			"description": "Facture à payer dans un mois",
+			"price": 300,
+			"paid": false,
+			"paid_list": [{
+				user: user,
+				price: 0
+			}, {
+				user: friend,
+				price: 0
+			}],
+			"date": new Date().setDate(new Date().getDate() + 30)
+		}],
+		users: [friend]
+	}).save()
+
+	new Group({
+		name: "Groupe 2",
+		owner: user._id,
+		bills: [{
+			"description": "Facture du group 2",
+			"price": 300,
+			"paid": false,
+			"paid_list": [{
+				user: user,
+				price: 0
+			}, {
+				user: friend,
+				price: 0
+			}],
+			"date": new Date().setDate(new Date().getDate() + 30)
+		}],
+		users: [friend]
+	}).save()
+})
